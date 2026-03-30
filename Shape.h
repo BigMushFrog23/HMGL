233b8723f9f2e8bed675f4d2952aa2030a2a6828
USER <151913504+BigMushFrog23@users.noreply.github.com>
2026-03-30 16:47:49 +0200

    Comments: Added comments to Shape for better understanding of the written code.

--- Shape.h
+++ Shape.h
@@ -12,8 +12,9 @@ public:
 	
 	Shape(int x, int y, uint32_t color) : m_x(x), m_y(y), m_color(color) {}
 
-	virtual ~Shape() = default;
-	virtual void Draw() = 0; // Pure virtual
+	// virtual means that Shape might be replaced by a child class later, so use the child's functions.
+	virtual ~Shape() = default; // ~Shape() = default is a destructor, it is run before the object is deleted to clean up. default writes a basic empty destructor for it.
+	virtual void Draw() = 0; // Pure virtual, Shape has no Draw code, so it will be forced to instead be a child class that has Draw code.
 
 };
 
@@ -24,5 +25,5 @@ private:
 
 public:
 	Square(int x, int y, int size, uint32_t color);
-	void Draw() override;
+	void Draw() override; // Use this Class' Draw function instead of the parent Shape, replacing the virtual Draw.
 };
